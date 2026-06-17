fadf6cd73b7f12ca46c9a49e8f6aa59fab6c2d56
K1-mikaze <K1mikaze@proton.me>
2026-06-17 10:29:55 -0500

    Fixing a been cycle

--- backend/src/main/java/nixdocs/backend/configuration/security/SecurityConfig.java
+++ backend/src/main/java/nixdocs/backend/configuration/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -43,6 +44,7 @@ public class SecurityConfig {
   private CustomOAuth2UserService customOAuth2UserService;
 
   @Autowired
+  @Lazy
   private AuthController authController;
 
   public SecurityConfig() {
